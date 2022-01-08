e84459e2df1db52715dc81aedc0e400cd6efc826
Andrew Gutekanst <andrew.gutekanst@gmail.com>
2022-01-08 18:50:26 -0500

    Add estimated REQ/RES/NTC packets by SubID

--- packet_docs/ddon_packet_md_gen.py
+++ packet_docs/ddon_packet_md_gen.py
@@ -16,11 +16,27 @@ with open('GamePackets.md', 'wt', encoding='utf8') as f:
         else:
             f.write(f"### Group: {group['GroupID']}\n")
 
-        f.write(f"|Name|GroupID|ID|Sub ID|Handler Address|Handler Comment|\n")
+        f.write(f"|Name|GroupID|ID|Sub ID|Handler Address|Comment|\n")
         f.write(f"|---|---|---|---|---|---|\n")
 
         for handler in group['Handlers']:
-            f.write(f"|{handler['PacketName']}|{group['GroupID']}|{handler['ID']}|{handler['SubID']}|{hex(handler['HandlerAddr'])}|{handler['HandlerComment']}|\n")
+            if handler['SubID'] == 2:
+                # Response packet, forge request packet :)
+                #'abc_5_0_2_RES'.replace('5_0_2', '5_0_1').removesuffix('_RES') + '_REQ'
+                req_ver_string = f"{group['GroupID']}_{handler['ID']}_1"
+                res_ver_string = f"{group['GroupID']}_{handler['ID']}_{handler['SubID']}"
+                req_pname = handler['PacketName'].replace(res_ver_string, req_ver_string).removesuffix('_RES') + '_REQ'
+                f.write(f"|{req_pname}|{group['GroupID']}|{handler['ID']}|1|N/A||\n")
+
+            output_pname = handler['PacketName']
+            if handler['SubID'] == 16 and not output_pname.endswith('_NTC'):
+                output_pname += '_NTC'
+
+            if handler['SubID'] == 2 and not output_pname.endswith('_RES'):
+                output_pname += '_RES'
+
+
+            f.write(f"|{output_pname}|{group['GroupID']}|{handler['ID']}|{handler['SubID']}|{hex(handler['HandlerAddr'])}|{handler['HandlerComment']}|\n")
             
 
 
@@ -36,11 +52,27 @@ with open('GamePackets.md', 'wt', encoding='utf8') as f:
             f.write(f"// Group: {group['GroupID']}\n")
 
         for handler in group['Handlers']:
-            #public static readonly PacketId L2C_GP_COURSE_GET_INFO_RES = new PacketId(4, 0, 2, "L2C_GP_COURSE_GET_INFO_RES");
+            if handler['SubID'] == 2:
+                # Response packet, forge request packet :)
+                #'abc_5_0_2_RES'.replace('5_0_2', '5_0_1').removesuffix('_RES') + '_REQ'
+                req_ver_string = f"{group['GroupID']}_{handler['ID']}_1"
+                res_ver_string = f"{group['GroupID']}_{handler['ID']}_{handler['SubID']}"
+                req_pname = handler['PacketName'].replace(res_ver_string, req_ver_string).removesuffix('_RES') + '_REQ'
+                f.write(f"|{req_pname}|{group['GroupID']}|{handler['ID']}|1|N/A||\n")
+                f.write(f"public static readonly PacketID {req_pname} = new PacketId({group['GroupID']}, {handler['ID']}, 1, \"{req_pname}\");\n")
+
+            output_pname = handler['PacketName']
+            if handler['SubID'] == 16 and not output_pname.endswith('_NTC'):
+                output_pname += '_NTC'
+
+            if handler['SubID'] == 2 and not output_pname.endswith('_RES'):
+                output_pname += '_RES'
+
+
             if 'HandlerComment' in handler and handler['HandlerComment'] is not None:
-                f.write(f"public static readonly GameServerPacketId {handler['PacketName']} = new PacketId({group['GroupID']}, {handler['ID']}, {handler['SubID']}, \"{handler['PacketName']}\"); // {handler['HandlerComment']}\n")
+                f.write(f"public static readonly GameServerPacketId {output_pname} = new PacketId({group['GroupID']}, {handler['ID']}, {handler['SubID']}, \"{output_pname}\"); // {handler['HandlerComment']}\n")
             else:
-                f.write(f"public static readonly GameServerPacketId {handler['PacketName']} = new PacketId({group['GroupID']}, {handler['ID']}, {handler['SubID']}, \"{handler['PacketName']}\");\n")
+                f.write(f"public static readonly GameServerPacketId {output_pname} = new PacketId({group['GroupID']}, {handler['ID']}, {handler['SubID']}, \"{output_pname}\");\n")
 
         f.write('\n')
 
